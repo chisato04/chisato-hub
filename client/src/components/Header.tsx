7e5fe63e2218fa17868d5a59c6fa8e4cdf8670aa
chisato04 <elmalinao20@gmail.com>
2025-11-05 14:58:03 +0800

    feat(client): replaced redundant github link in header with discord link

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -12,7 +12,7 @@ const Header: React.FC = () => {
           <ul>
             <li><NavLink to="/modpacks">Packs</NavLink></li>
             <li><NavLink to="/gallery">Gallery</NavLink></li>
-            <li><a href="https://github.com/chisato04/chisato-hub" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+            <li><a href="https://discord.gg/MFHcDdbT" target="_blank" rel="noopener noreferrer">Discord</a></li>
           </ul>
         </nav>
       </div>
